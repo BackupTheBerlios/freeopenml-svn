7f03badcdad5cf8acb7f49411a74ecbd22792874
cityhunter <cityhunter@823dff18-93dc-0310-951f-ee5f0099c243>
2005-04-01 15:19:58 +0000

    cleaned source tree

--- tags/init/freeOpenML/include/OpenML/temp.h
+++
@@ -1,9 +0,0 @@
-/***********************************************************************
-* copyright (c) 2004 MALET Jean-luc aka cityhunter
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the artistic license as published in the top source dir of the
-* package
-************************************************************************/
-
-
-
